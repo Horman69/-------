3b34aefdf47552ab14108ca19500c0955aeabb4f
0x404f <46415372+Horman69@users.noreply.github.com>
2023-11-10 11:25:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// Получаем элемент canvas и его 2D-контекст
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+// Устанавливаем ширину и высоту canvas, равные размерам окна браузера
+const canvasWidth = (canvas.width = window.innerWidth);
+const canvasHeight = (canvas.height = window.innerHeight);
+
+// Задаем строку символов, которую будем отображать
+const text = "А+Б0В-Г1Д=Е2Ё Ж3З И4Й К5Л М6Н О7П Р8С Т9У Ф!Х Ц?Ч Ш.ЩЪ,Ы Ь:ЭЮ;Я";
+const characters = text.split(""); // Разбиваем строку на массив символов
+
+// Инициализируем переменные для анимации
+const fontSize = 11; // Размер шрифта
+const columnCount = canvasWidth / fontSize; // Количество столбцов
+const characterPositions = [];
+
+for (let i = 0; i < columnCount; i++) {
+  characterPositions[i] = 1; // Инициализируем начальные вертикальные позиции символов
+}
+
+// Функция для отрисовки анимации
+function draw() {
+  // Очищаем canvas с небольшой непрозрачностью, чтобы создать эффект плавного исчезания символов
+  context.fillStyle = "rgba(0,0,0,.05)";
+  context.fillRect(0, 0, canvasWidth, canvasHeight);
+
+  context.fillStyle = "#0f0"; // Задаем цвет для символов (зеленый)
+  context.font = fontSize + "px system-ui"; // Задаем размер и шрифт
+
+  for (let i = 0; i < characterPositions.length; i++) {
+    // Для каждого столбца
+    const randomCharacter = characters[Math.floor(Math.random() * characters.length)]; // Выбираем случайный символ из массива
+    context.fillText(randomCharacter, i * fontSize, characterPositions[i] * fontSize); // Рисуем символ в текущей позиции
+
+    if (characterPositions[i] * fontSize > canvasHeight && Math.random() > 0.975) {
+      characterPositions[i] = 0; // Если символ достиг нижней границы и выполняется условие, переносим его наверх
+    }
+    characterPositions[i]++; // Увеличиваем вертикальную позицию символа
+  }
+}
+
+// Запускаем функцию draw() с интервалом для создания анимации
+setInterval(draw, 123);
+
+// Обработчик события изменения размера окна браузера, перезагружающий страницу
+window.addEventListener("resize", () => location.reload());
